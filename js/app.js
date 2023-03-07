2597283a96cbf6bdf4be9ab2712cd64c92afa54a
emilymerrow <emilyanddavidmerrow@gmail.com>
2023-03-07 14:46:35 -0500

    Add pixel work

--- js/app.js
+++ js/app.js
@@ -1,4 +1,52 @@
-
+let characterTemplate = {
+    0: [
+      { x: 9, y: 9, color: '#fefcfc' },
+      { x: 10, y: 9, color: '#fefcfc' },
+      { x: 11, y: 9, color: '#fefcfc' },
+      { x: 12, y: 10, color: '#fefcfc' },
+      { x: 13, y: 11, color: '#fefcfc' },
+      { x: 13, y: 12, color: '#fefcfc' },
+      { x: 12, y: 13, color: '#fefcfc' },
+      { x: 11, y: 14, color: '#fefcfc' },
+      { x: 10, y: 14, color: '#fefcfc' },
+      { x: 9, y: 14, color: '#fefcfc' },
+      { x: 8, y: 13, color: '#fefcfc' },
+      { x: 7, y: 12, color: '#fefcfc' },
+      { x: 7, y: 11, color: '#fefcfc' },
+      { x: 8, y: 10, color: '#fefcfc' },
+      { x: 9, y: 9, color: '#fefcfc' },
+      { x: 9, y: 11, color: '#fefcfc' },
+      { x: 11, y: 11, color: '#fefcfc' },
+    ],
+    1: [
+      { x: 9, y: 9, color: '#fefcfc' },
+      { x: 10, y: 9, color: '#fefcfc' },
+      { x: 11, y: 9, color: '#fefcfc' },
+      { x: 12, y: 9, color: '#fefcfc' },
+      { x: 13, y: 10, color: '#fefcfc' },
+      { x: 14, y: 11, color: '#fefcfc' },
+      { x: 14, y: 12, color: '#fefcfc' },
+      { x: 13, y: 13, color: '#fefcfc' },
+      { x: 11, y: 14, color: '#fefcfc' },
+      { x: 12, y: 14, color: '#fefcfc' },
+      { x: 10, y: 14, color: '#fefcfc' },
+      { x: 9, y: 14, color: '#fefcfc' },
+      { x: 8, y: 13, color: '#fefcfc' },
+      { x: 7, y: 12, color: '#fefcfc' },
+      { x: 7, y: 11, color: '#fefcfc' },
+      { x: 8, y: 10, color: '#fefcfc' },
+      { x: 9, y: 9, color: '#fefcfc' },
+      { x: 9, y: 11, color: '#fefcfc' },
+      { x: 12, y: 11, color: '#fefcfc' },
+    ]
+  
+  }
+  
+  let config = {
+    width: 20,
+    height: 20,
+    color: 'white',
+  }
 
 let character;
 let statsIncreaseInterval = 5000;
@@ -8,8 +56,11 @@ let boredom = 1;
 let age = 0;
 let isDead = false;
 
+document.getElementById('pixel-screen').style.width = `calc(${(0.825 * config.width)}rem + ${(config.height * 3)}px)`;
+document.getElementById('pixel-screen').style.height = `calc(${(0.825 * config.height)}rem + ${(config.width * 3)}px)`;
 
-class Tomagotchi {
+
+class Tomagotchi {  //T class with a property (name)
 
     constructor(name) {
       this.name = name;
@@ -17,31 +68,32 @@ class Tomagotchi {
   
   }
   
-  let name = prompt("Please enter your pet's name.");
-  character = new Tomagotchi(name);
+  let name = prompt("Please enter your pet's name."); //sets up the initial state of the T game 
+  character = new Tomagotchi(name);                  //and creates a new T object with user's pet name
   
-  document.getElementById("character-name").innerHTML = character.name;
-  document.getElementById("character-age").innerHTML = `(Age:${age})`;
+  document.getElementById("character-name").innerHTML = character.name; //updates the text content of the HTML elem. with the ID attibutes
+  document.getElementById("character-age").innerHTML = `(Age:${age})`;  // to show the name & age of the T the user created
   
   
-  const displayUpdatedStats = () => {
-    if(isDead) {
+  const displayUpdatedStats = () => { //this funct. updates the HTML content of hunger, sleepiness, boredom
+    if(isDead) {                     //
       return;
     }
-    document.getElementById("hunger-stat").innerHTML = `(${hunger})`;
-    document.getElementById("sleepiness-stat").innerHTML = `(${sleepiness})`;
+    document.getElementById("hunger-stat").innerHTML = `(${hunger})`; //getElementById() method is used to select the HTML elements
+    document.getElementById("sleepiness-stat").innerHTML = `(${sleepiness})`; //.innerHTML property is used to update their content
     document.getElementById("boredom-stat").innerHTML = `(${boredom})`;
-    if (hunger === 10 || sleepiness === 10 || boredom === 10) {
-      isDead = true;
+    if (hunger === 10 || sleepiness === 10 || boredom === 10) { //if any of the levels reached 10, isDead=true
+      isDead = true; //prevent further updates ont the T stats after it died
       drawDead();
     }
   }
   
-  const decreaseHunger = () => {
+  const decreaseHunger = () => { //this funct. decreases hunger levels if T levels is >=2
     if (hunger >= 2) {
       hunger --;
-      displayUpdatedStats();
-    }
+      displayUpdatedStats(); //calls the funct. to update the HTML content of the Tamagotchi's stats
+    }                        //after the hunger level is decreased
+}                        
   }
   
   const decreaseSleepiness = () => {
@@ -57,11 +109,12 @@ class Tomagotchi {
       displayUpdatedStats();
     }
   }
-  
+  //generates a random number between 0 and 1 using the Math.random() method, 
+  // x2, rounds it down to the nearest integer using the Math.floor() method
   const animateCharacter = () => {
-    draw(Math.floor(Math.random() * 2));
-  }
-  
+    draw(Math.floor(Math.random() * 2)); //creates the illusion of T moving or
+  }                                      //changing direction on the screen
+               
 
 
 
